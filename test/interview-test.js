fcb065bbd5c63d4096e0d5821342b2af1d3fc758
bcoe <bencoe@gmail.com>
2014-07-04 00:28:04 -0700

    refactoring reading/writing of service.json, to DRY up code.

--- test/interview-test.js
+++ test/interview-test.js
@@ -1,7 +1,10 @@
 require('../lib/config')({headless: true}); // turn off output in tests.
 
 var Lab = require('lab'),
-  Interview = require('../lib').Interview;
+  Interview = require('../lib').Interview,
+  Installer = require('../lib/').Installer,
+  rimraf = require('rimraf'),
+  fs = require('fs');
 
 Lab.experiment('interview', function() {
 
@@ -65,6 +68,54 @@ Lab.experiment('interview', function() {
       Lab.expect(
         interview.questionLookup['ndm-test:args:--frontdoor-url'].message
       ).to.eql('pork chop sandwiches');
+
+      done();
+    });
+  });
+
+  Lab.experiment('run', function() {
+
+    Lab.beforeEach(function(done) {
+      rimraf.sync('./test/fixtures/service.json');
+      rimraf.sync('./test/fixtures/logs');
+      done();
+    });
+
+    Lab.after(function(done) {
+      rimraf.sync('./test/fixtures/service.json');
+      rimraf.sync('./test/fixtures/logs');
+      done();
+    });
+
+    Lab.it('persists service.json with answers to questions', function(done) {
+      // generate the service.json.
+      var installer = new Installer({
+        baseWorkingDirectory: './test/fixtures'
+      });
+
+      installer.init();
+
+      // run the interview.
+      var interview = new Interview({
+        serviceJson: './test/fixtures/service.json',
+        inquirer: {
+          prompt: function(questions, cb) {
+            cb({
+              overwrite: true,
+              '--host': '2.2.2.2'
+            })
+          }
+        }
+      });
+
+      interview.run(function() {});
+
+      // validate the service.json written.
+      var serviceJson = JSON.parse(
+        fs.readFileSync('./test/fixtures/service.json').toString()
+      );
+
+      Lab.expect(serviceJson['ndm-test'].args['--host']).to.eql('2.2.2.2');
 
       done();
     });
