bd6a89b766f1760b13e9eaefed466255fc87d714
bcoe <bencoe@gmail.com>
2014-07-28 18:10:43 -0700

    we now exit with 1 if an error has been logged.

--- bin/ndm.js
+++ bin/ndm.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
 
 require('../lib/cli')().run();
+
+// emit the appropriate exit code before shutting down.
+process.on('exit', function() {
+  if (require('../lib/logger').errorLogged()) process.exit(1);
+  else process.exit(0);
+});
