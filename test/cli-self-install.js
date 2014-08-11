76da14f2ae8d76d0fc44a9804d4b09498c77151d
bcoe <bencoe@gmail.com>
2014-08-10 20:31:53 -0700

    we now have a self-install api, hooray.

---
+++ test/cli-self-install.js
@@ -0,0 +1,23 @@
+require('../lib/config')({headless: true}); // turn off output in tests.
+
+var Lab = require('lab'),
+  path = require('path'),
+  _ = require('lodash');
+
+Lab.experiment('cli-self-install', function() {
+  Lab.experiment('init', function() {
+    Lab.it('should infer module name based on package.json', function(done) {
+      var ndm = require('../lib')('ndm-test');
+      Lab.expect(ndm.serviceName).to.eql('ndm-test');
+      done();
+    });
+
+    Lab.it('should allow api opts to be overridden', function(done) {
+      var ndm = require('../lib')('ndm-test', {
+        logsDirectory: '/foo/logs'
+      });
+      Lab.expect(ndm.logsDirectory).to.eql('/foo/logs');
+      done();
+    });
+  });
+});
